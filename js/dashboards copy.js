83645ce247f7808dd9e4353297cac86023f83627
5uRFi <cronos_kaos@hotmail.com>
2021-01-25 01:27:32 -0600

    merge: graficas por categoria y anio

--- js/dashboards copy.js
+++ js/dashboards copy.js
@@ -7,7 +7,7 @@ alto_total = ancho_total * 9 / 16
 graf.style('width', `${ancho_total}px`)
     .style('height',`${alto_total}px`)
 
-margins = {top: 50, left:70, right:15, bottom:50}
+margins = {top: 50, left:50, right:15, bottom:50}
 
 ancho = ancho_total - margins.left - margins.right
 alto  = alto_total - margins.top - margins.bottom
@@ -35,25 +35,22 @@ x = d3.scaleBand()
 color = d3.scaleOrdinal()
           .range(d3.schemeDark2)
 
-banco = d3.scaleBand()
-
 // -- Grupos/capas ---
 
 //yAxGroup = g.append('g')
 
-yAxisGroup = g.append('g')
-              .attr('class', 'ejes')
-
 xAxisGroup = g.append('g')
               .attr('transform', `translate(0, ${ alto })`)
-              .attr('class', 'ejes')
+              .attr('class', 'eje')
+yAxisGroup = g.append('g')
+              .attr('class', 'eje')
 
 
 /////////////////////inicia one////////////////////////77
 
 // (1) Variables globales para determinar que mostrar y
 //     poder obtener los datos del select
-comboAnios = 'todos'
+comboAnios = 'todas'
 anioSelect = d3.select('#anio')
 
 metrica = 'solicitudes'
@@ -63,9 +60,9 @@ ascendente = false
 
 xAxisGroup = g.append('g')
               .attr('transform', `translate(0, ${ alto })`)
-              .attr('class', 'ejes')
+              .attr('class', 'eje')
 yAxisGroup = g.append('g')
-              .attr('class', 'ejes')
+              .attr('class', 'eje')
 
 titulo = g.append('text')
           .attr('x', `${ancho / 2}px`)
@@ -97,89 +94,57 @@ function render(data) {
 */
 
 function render(data) {
-
-    if(comboAnios == "todos"){
-        bars =  g.selectAll('rect')
-                .data(data)
-        // Barras
-        bars.enter()
-            .append('rect')
-                .style('width', '0px')
-                .style('height', '0px')
-                .style('y', `${y(0)}px`)
-                .style('fill', '#eaeae1')
-                .style('x', d => x(d.solicitudes) + 'px')
-        .transition()
-        //.ease(d3.easeElastic)
-        .duration(1500)
-                .style('y', d => `${(y(d.solicitudes))}px`)
-                .style('height', d => `${alto - y(d.solicitudes)}px`)
-                .style('fill', d => color(d.anio))
-                .style('width', d => `${x.bandwidth()}px`)
-                
-        // Ejes
-        yAxCall = d3.axisLeft(y)
-                    .tickFormat(d => `${d} m`)
-        yAxisGroup.call(yAxCall) 
-        banco.domain(data.map(d => d.banco))
-            .range([0, ancho])
-            .padding([0.2])
-        xAxCall = d3.axisBottom(banco)  
-        xAxisGroup.call(xAxCall)
-                .selectAll('text')
-                .attr('x', '0')
-                .attr('y', '10')
-                .attr('text-anchor', 'middle')
-
-    }else{    
-        bars = g.selectAll('rect')
-                .data(data, d => d.banco)
-
-        bars.enter()
-            .append('rect')
-            .style('width', '0px')
-            .style('height', '0px')
-            .style('y', `${y(0)}px`)
-            .style('fill', '#000')
+    // function(d, i) { return d }
+    // (d, i) => d
+  
+    bars = g.selectAll('rect')
+              .data(data, d => d.banco)
+  
+    bars.enter()
+        .append('rect')
+          .style('width', '0px')
+          .style('height', '0px')
+          .style('y', `${y(0)}px`)
+          .style('fill', '#000')
+          .style('x', d => x(d.banco) + 'px')
+        .merge(bars)
+          .transition()
+          // https://bl.ocks.org/d3noob/1ea51d03775b9650e8dfd03474e202fe
+          // .ease(d3.easeElastic)
+          .duration(2000)
             .style('x', d => x(d.banco) + 'px')
-            .merge(bars)
-            .transition()
-            // https://bl.ocks.org/d3noob/1ea51d03775b9650e8dfd03474e202fe
-            // .ease(d3.easeElastic)
-            .duration(2000)
-                .style('x', d => x(d.banco) + 'px')
-                .style('y', d => (y(d[metrica])) + 'px')
-                .style('height', d => (alto - y(d[metrica])) + 'px')
-                .style('fill', d => color(d.banco))
-                .style('width', d => `${x.bandwidth()}px`)
-    
-        bars.exit()
-            .transition()
-            .duration(2000)
-            .style('height', '0px')
-            .style('y', d => `${y(0)}px`)
-            .style('fill', '#000000')
-            .remove()
-    
-    
-        yAxisCall = d3.axisLeft(y)
-                    .ticks(3)
-                    .tickFormat(d => d + ((metrica == 'monto') ? 'm.' : ''))
-        yAxisGroup.transition()
-                .duration(2000)
-                .call(yAxisCall)
-    
-        xAxisCall = d3.axisBottom(x)
-        xAxisGroup.transition()
-                .duration(2000)
-                .call(xAxisCall)
-                .selectAll('text')
-                .attr('x', '0px')
-                .attr('y', '10px')
-                .attr('text-anchor', 'middle')
-                //.attr('transform', 'rotate(-90)')
+            .style('y', d => (y(d[metrica])) + 'px')
+            .style('height', d => (alto - y(d[metrica])) + 'px')
+            .style('fill', d => color(d.banco))
+            .style('width', d => `${x.bandwidth()}px`)
+  
+    bars.exit()
+        .transition()
+        .duration(2000)
+          .style('height', '0px')
+          .style('y', d => `${y(0)}px`)
+          .style('fill', '#000000')
+        .remove()
+  
+  
+    yAxisCall = d3.axisLeft(y)
+                  .ticks(3)
+                  .tickFormat(d => d + ((metrica == 'monto') ? 'm.' : ''))
+    yAxisGroup.transition()
+              .duration(2000)
+              .call(yAxisCall)
+  
+    xAxisCall = d3.axisBottom(x)
+    xAxisGroup.transition()
+              .duration(2000)
+              .call(xAxisCall)
+              .selectAll('text')
+              .attr('x', '0px')
+              .attr('y', '10px')
+              .attr('text-anchor', 'middle')
+              //.attr('transform', 'rotate(-90)')
 
-    }
+              
   }
 
 // Carga de datos - los montos se escalan a miles de millones y el nro de opns a miles
@@ -196,8 +161,8 @@ d3.csv('/data/DatasetTarjetasDebito.csv').then(function (data) {
     distinctYears = new Set(anioCombo);
 
     anioSelect.append('option')
-              .attr('value', 'todos')
-              .text('Todos')
+              .attr('value', 'todas')
+              .text('Todas')
 
     distinctYears.forEach(d => {
     anioSelect.append('option')
@@ -220,7 +185,7 @@ d3.csv('/data/DatasetTarjetasDebito.csv').then(function (data) {
 function frame() {
 
     dataframe = dataArray
-    if (comboAnios != 'todos') {
+    if (comboAnios != 'todas') {
       console.log("anios"+comboAnios)
       dataframe = d3.filter(dataArray, d => d.anio == comboAnios)
     }
@@ -242,12 +207,13 @@ function frame() {
     // Creamos una función para calcular la altura
     // de las barras y que quepan en nuestro canvas
     y.domain([0, nroSolsMax])
+    x.domain(dataframe.map(d => d.banco))
 
-    if(comboAnios == "todos"){
-        x.domain(dataframe.map(d => d.solicitudes))
-    }else{
-        x.domain(dataframe.map(d => d.banco))    
-    }
+    //nroSolsMax = d3.max(data, d => d.solicitudes)
+    //y.domain([0, nroSolsMax])
+    //x.domain(data.map(d => d.solicitudes))
+    //x.domain(dataframe.map(d => d.solicitudes))
+    //render(dataArray)
   
     render(dataframe)
 }
